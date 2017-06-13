ed6f172bef499b57d827c2504a8c1f32c5998151
ehsannikkhah <ehsanenikkhah@yahoo.com>
2017-06-13 15:25:07 +0430

    Created by Coding Ground Client

---
+++ OOSD/Employee.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Employee {
+  
+  private String name ;
+  private Date hireDate ;
+
+  
+  public void Employee(String name , Date hireDate ){
+    
+    this.name = name ; 
+    this.hireDate = hireDate ;
+  
+  }
+}
