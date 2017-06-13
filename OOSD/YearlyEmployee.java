fc65e9cbadd7d38442f4bdef32a1fc928e1a5908
ehsannikkhah <ehsanenikkhah@yahoo.com>
2017-06-13 15:27:02 +0430

    Created by Coding Ground Client

---
+++ OOSD/YearlyEmployee.java
@@ -0,0 +1,10 @@
+public class YearlyEmployee extends Employee {
+  
+  private double employeeSalary;
+  
+  public YearlyEmployee (String name1 ,Date date1 , double salary ){
+    
+    super(name,date);
+  }
+
+}
